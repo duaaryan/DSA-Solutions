8febd9b6df633b46ccf56f1ed35668b1b33af220
Aryan Dua <duaaryan14@gmail.com>
2026-08-20 15:23:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        int n = arr.size();
+        int low =0;
+        int high = n-1;
+        while (low <=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>arr[high]){
+                low = mid +1;
+            }
+            else {
+                high=mid;
+            }
+            if(low==high){
+                return arr[low];
+            }
+            
+        }
+        
+    }
+};
