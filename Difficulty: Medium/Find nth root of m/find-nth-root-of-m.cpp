ab8b371446ae69e7bd324c364a8329e21e4e3711
Aryan Dua <duaaryan14@gmail.com>
2026-08-22 18:05:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  int func(int mid, int n, int m){
+      long long ans=1;
+        for(int i =1;i<=n;i++){
+             ans =ans*mid;
+            if(ans> m) return 2;
+             
+        }
+        if(ans==m) return 1;
+         return 0;
+  }
+    int nthRoot(int n, int m) {
+       int low =0;
+       int high = m;
+       while(low<=high){
+           int mid = (low+high)/2;
+           int midN = func(mid, n, m);
+           if(midN==1){
+               return mid;
+           }
+           else if(midN==0) low = mid+1;
+           else high =mid-1;
+       }
+       return -1; 
+    }
+};
