3532b451be4558ab4844063a2d8980aa4cd048c9
Aryan Dua <duaaryan14@gmail.com>
2026-08-28 17:06:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+  int mallu(vector<int>&arr ,long long maxsum){
+      int n = arr.size();
+      int bro = 1;
+      long long bro1 =arr[0];
+      for(int i =1;i<n;i++){
+          if(bro1 +arr[i] <= maxsum){
+              bro1 += arr[i];
+          }
+          else{
+              bro++;
+              bro1 = arr[i];
+          }
+      }
+      return bro;
+  }
+    int splitArray(vector<int>& arr, int k) {
+       long long low = *max_element(arr.begin(), arr.end());
+       long long high = accumulate(arr.begin(), arr.end(), 0ll);
+       int ans = -1;
+       while(low<=high){
+           long long mid = (low + high)/2;
+           int mal = mallu(arr,mid );
+           if(mal >k){
+               low = mid +1;
+           }
+           else{
+               ans = mid;
+               high = mid -1;
+               
+           }
+       }
+       return ans;
+    }
+};
