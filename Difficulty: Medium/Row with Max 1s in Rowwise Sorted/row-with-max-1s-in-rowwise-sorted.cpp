8842abe1a9df5e4e7f8b31de0d3c09b6b3a05aee
Aryan Dua <duaaryan14@gmail.com>
2026-09-01 23:48:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+       int n = arr.size();
+       int m = arr[0].size();
+       int maxcount = 0;
+       int ans =-1;
+       for(int i =0;i<n;i++){
+           int count = m -(lower_bound(arr[i].begin(), arr[i].end(), 1) - arr[i].begin());
+           if(count > maxcount){
+               maxcount = count;
+               ans =i;
+           }
+       }
+       return ans;
+        
+    }
+};
