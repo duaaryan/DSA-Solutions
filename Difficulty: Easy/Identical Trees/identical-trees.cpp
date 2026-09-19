f2ff6bc91da14d48660fb92b6007b76635b4c3b2
Aryan Dua <duaaryan14@gmail.com>
2026-09-19 19:19:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool isIdentical(Node* r1, Node* r2) {
+       if(r1 == nullptr || r2 == nullptr){
+           return (r1==r2);
+       }
+       return (r1->data == r2 ->data)
+       && isIdentical(r1->left, r2->left)
+       && isIdentical(r1->right, r2->right);
+        
+    }
+};
